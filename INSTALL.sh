2dbede23a8caca2bb4c8ba4fb089cbef3b2e2d14
Thomas Nash <null@endmail.xyz>
2020-09-14 15:35:20 +1000

    font installing

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-mkdir -p /usr/share/fonts/truetype/prefect
-unzip "*.zip" -d /usr/share/fonts/truetype/prefect
-mkfontdir /usr/share/fonts
-mkfontscale /usr/share/fonts
+mkdir -p /usr/share/fonts/truetype/helvetica
+unzip "*.zip" -d /usr/share/fonts/truetype/helvetica
 sudo fc-cache -fv
